5b15a701080c4288952ba3362e46be32a2e622e6
Anirudh Uppal <10473964+anuppal@users.noreply.github.com>
2022-12-08 02:01:20 +0530

    Day-07-12-2022 reactJS training from Udemy Section-3

---
+++ reactcompleteguide/src/components/Expenses.js
@@ -0,0 +1,47 @@
+// import App from "../App";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css"
+
+function Expense(props) {
+  return (
+
+    // Method-1
+    <div className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
+    </div>
+    
+    //Method - 2 
+
+        // <div className="expenses">
+        //     {props.items &&
+        //         props.items.map((item) => (
+        //             <ExpenseItem
+        //                 title={item.title}
+        //                 amount={item.amount}
+        //                 date={item.date}
+        //                 key={item.id}
+        //             />
+        //         ))}
+        // </div>
+  );
+}
+export default Expense;
